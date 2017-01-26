d7fa072d911f13706f823e489792d6608b0112f8
FateEduardo <a338694@uabc.edu.mx>
2017-01-25 21:48:00 -0800

    DAO-SQL

--- src/main/java/com/softtek/academy/domain/ShipTo.java
+++ src/main/java/com/softtek/academy/domain/ShipTo.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
@@ -25,8 +27,13 @@ public class ShipTo implements Serializable{
 	@Column(name = "ship_to_id")
 	private Long id;
 	
+	@JoinColumn(name="user")
+	@ManyToOne
+	private User user;
+	
 	@Column(name = "name")
 	private String name;
+	
 	
 	@Column(name = "address")
 	private String address;
